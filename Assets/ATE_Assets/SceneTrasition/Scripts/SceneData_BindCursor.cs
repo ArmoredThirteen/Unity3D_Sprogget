f91512984aa38f6a1b5fddba609685dad059ba02
ArmoredThirteen <davicram+github@gmail.com>
2014-01-31 02:43:32 -0800

    Locked Cursor
    Added in a basic cursor-locking script.

---
+++ Assets/ATE_Assets/SceneTrasition/Scripts/SceneData_BindCursor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneData_BindCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this);
+		Screen.lockCursor = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// exit condition for locked cursor
+		if (Input.GetKeyDown ("escape"))
+			Screen.lockCursor = false;
+	}
+
+
+	void OnMouseDown() {
+		Screen.lockCursor = true;
+	}
+}
